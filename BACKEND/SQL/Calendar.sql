9da9bb026275e400d155013dd98a93d2812607fc
Lucia Beccaro <lucia.beccaro1@gmail.com>
2023-04-12 10:08:32 +0200

    INDEX.

--- BACKEND/SQL/Calendar.sql
+++ BACKEND/SQL/Calendar.sql
@@ -1,27 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Creato il: Apr 05, 2023 alle 13:45
--- Versione del server: 10.4.27-MariaDB
--- Versione PHP: 8.2.0
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `Calendar`
---
-
--- --------------------------------------------------------
 
 --
 -- Struttura della tabella `attività`
@@ -128,19 +109,10 @@ ALTER TABLE `scadenze`
 ALTER TABLE `utente`
   ADD PRIMARY KEY (`email`);
 
---
--- Limiti per le tabelle scaricate
---
 
---
--- Limiti per la tabella `attività`
 --
 ALTER TABLE `attività`
   ADD CONSTRAINT `attività_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `categorie` (`id`),
   ADD CONSTRAINT `attività_ibfk_2` FOREIGN KEY (`priorità`) REFERENCES `priorità` (`id`),
   ADD CONSTRAINT `attività_ibfk_3` FOREIGN KEY (`scadenza`) REFERENCES `scadenze` (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
